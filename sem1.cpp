c74ce4ad8618ac2930a073a832c8011d1a76ce9d
Program <Program@MacBook-Pro.local>
2024-09-21 23:36:22 +0300

    first commit

---
+++ sem1.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+#include<fstream>
+
+int main(int argc, char* argv[]){
+    int n = atoi(argv[1]);
+    std::ofstream file;
+    file.open("output.txt");
+    for (int i = 0; i<n; i++){
+        file << i << std::endl;
+    }
+    file.close();
+    return 0;
+}
