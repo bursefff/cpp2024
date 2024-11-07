82ae9396d822db3af8e51a9ceeff8c596ba0ee0d
Program <Program@MacBook-Pro.local>
2024-11-07 23:47:49 +0300

    test

---
+++ sem3.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<math.h>
+#include<bitset>
+
+
+int main(){
+    float a = 0;
+    for (int i = 1; i < 30; i++){
+        a += 1 / pow(2, i);
+        std::cout << std::bitset<32>(*reinterpret_cast<long*>(&a)) << std::endl;
+    }
+    std::cout<<a << std::endl;
+    
+    return 0;
+}
---
+++ sem32.py
@@ -0,0 +1,17 @@
+﻿import subprocess
+from math import sin
+from matplotlib import pyplot as plt
+import numpy as np
+
+fig, ax = plt.subplots()
+subprocess.run(["g++", "sem32.cpp", "-o", "sem32.out"])
+x = np.arange(0, 50, 0.1)
+y = np.vectorize(sin)(x)
+y1 = np.zeros(x.size)
+for i in range(x.size):
+    cp = subprocess.run(["./sem32.out", "1000000", str(x[i])], capture_output=True, text=True)
+    y1[i] = float(cp.stdout)
+
+# ax.plot(x, y)
+ax.plot(x, y1)
+plt.show()
